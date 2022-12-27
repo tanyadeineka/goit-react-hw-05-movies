dbad5a068b44fee68a7f65bd4c26269b27befe68
Tetiana Deineka <tata4ka666@i.ua>
2022-12-27 23:38:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { SharedLayout } from './SharedLayout/SharedLayout';
+import { SharedLayout } from './SharedLayout';
 import { lazy } from "react";
 
 const Home = lazy(() => import('../pages/Home'));
