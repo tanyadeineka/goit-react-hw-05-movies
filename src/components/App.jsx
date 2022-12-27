1894c07a17ee36bf9354a5f04ff585b80caed0e1
Tetiana Deineka <tata4ka666@i.ua>
2022-12-27 23:05:15 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { lazy } from "react";
+
+const Home = lazy(() => import('../pages/Home'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<></>} />
+        <Route path="movies/:id" element={<></>}>
+          <Route path="cast" element={<></>} />
+          <Route path="reviews" element={<></>} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
