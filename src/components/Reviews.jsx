affb91100c42c9a4f90a2e30f046bbe0b6010054
Tetiana Deineka <tata4ka666@i.ua>
2022-12-27 23:29:42 +0200

    Create Reviews.jsx

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,51 @@
+import { getReviews } from 'api';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Loader from './Loader';
+import css from './Reviewes.module.css';
+
+export default function Reviews() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [reviews, setReviews] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function onGetReviews() {
+      try {
+        setIsLoading(true);
+        const reviewes = await getReviews(id);
+        setReviews(reviews);
+      } catch {
+        setError("We don't have any review about this movie");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    onGetReviews();
+  }, [id]);
+  return (
+    <>
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
+      {reviews.length === 0 ? (
+        <p className={css.notification}>
+          We don't have any reviews about this movie
+        </p>
+      ) : (
+        <ul className={css.list}>
+          {reviews.map(review => {
+            return (
+              <li className={css.item} key={review.reviewId}>
+                <h1>{review.author}</h1>
+                <p>{review.content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
+}
