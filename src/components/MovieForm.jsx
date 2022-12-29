843d7c71bb91721ea872c8b0117d69617bc5e82c
Tetiana Deineka <tata4ka666@i.ua>
2022-12-29 21:31:41 +0200

    Update MovieForm.jsx

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,10 +1,19 @@
 import css from './MovieForm.module.css';
 import { ReactComponent as SearchIcon } from '../svg/Search.svg';
+import { useSearchParams } from 'react-router-dom';
 
-export const MovieForm = ({ onSubmit, value }) => {
+export const MovieForm = () => {
+  const [, setSearchParams] = useSearchParams();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    setSearchParams({ query: evt.target.elements.query.value });
+    evt.target.reset();
+  };
+
   return (
     <div className={css.searchbar}>
-      <form onSubmit={onSubmit} className={css.form}>
+      <form onSubmit={handleSubmit} className={css.form}>
         <button type="submit" className={css.button}>
           <span className={css.buttonLabel}></span>
           <SearchIcon width="20" height="20" className={css.searchSvg} />
