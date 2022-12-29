557bc29b8aae94ec70aa3f64b15cef1aff3f9d94
Tetiana Deineka <tata4ka666@i.ua>
2022-12-29 21:31:18 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
 import { getTrending } from "API";
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import Loader from "../components/Loader";
 import css from "./Home.module.css";
+import MovieList from "components/MovieList";
 
 export default function Home() {
     const [isLoading, setIsLoading] = useState(false);
     const [films, setFilms] = useState([]);
     const [error, setError] = useState(null);
-    const location = useLocation();
-
+    
     useEffect(() => {
         async function getTrendingFilms() {
             try {
                 setIsLoading(true);
                 const films = await getTrending();
-                setFilms(films);
+              setFilms(films);
+              setError(null);
             } catch {
                 setError('Sorry, we can not get data.');
             } finally {
@@ -29,14 +29,7 @@ export default function Home() {
         <h1 className={css.homeTitle}>Trending today</h1>
         {error && <p>{error}</p>}
         {isLoading && <Loader />}
-        <ul className={css.list}>
-          {films.map(film => (
-            <li key={film.id} className={css.item}>
-              <img src={`https://image.tmdb.org/t/p/w300/${film.poster}`} alt={film.title} width={300}></img>
-                  <Link className={css.link} to={`movies/${film.id}`} state={{from: location}}>{film.title}</Link>
-            </li>
-          ))}
-        </ul>
+        {films.length > 0 && <MovieList films={films} />}
       </div>
     );
 }
