baf7f3bd42aea6ced69e3ef9ed8559567def3804
Tetiana Deineka <tata4ka666@i.ua>
2022-12-29 21:31:14 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,16 +1,12 @@
-import { useEffect, useState, Suspense, useMemo } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { Outlet, useParams, Link, useLocation } from 'react-router-dom';
 import { getDetails } from 'API';
 import css from './MovieDetails.module.css';
 import Loader from '../components/Loader';
+import Info from 'components/Info';
 
 export default function MovieDetails() {
-  const [image, setImage] = useState('');
-  const [title, setTitle] = useState('');
-  const [date, setDate] = useState('');
-  const [score, setScore] = useState('');
-  const [overview, setOverview] = useState('');
-  const [genres, setGenres] = useState([]);
+  const [movieInfo, setMovieInfo] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
@@ -21,14 +17,8 @@ export default function MovieDetails() {
     async function getFilmDetails() {
       try {
         setIsLoading(true);
-        const { image, title, date, score, overview, genres } =
-          await getDetails(id);
-        setImage(image);
-        setTitle(title);
-        setDate(date.substr(0, 4));
-        setScore(String(Math.ceil(Number(score) * 10)));
-        setOverview(overview);
-        setGenres(genres);
+        const data = await getDetails(id);
+        setMovieInfo(data);
       } catch {
         setError('Sorry, we can not get data.');
       } finally {
@@ -38,49 +28,14 @@ export default function MovieDetails() {
     getFilmDetails();
   }, [id]);
 
-  const genresList = useMemo(() => {
-    const genreArray = [];
-    genres.map(genre => genreArray.push(genre.name));
-    return genreArray.join(', ');
-  }, [genres]);
-
   return (
     <>
       <Link className={css.goBack} to={backLinkHref}>
         Go back
       </Link>
-      <div className={css.descriptionWrapper}>
-        {error && <p>{error}</p>}
-        {isLoading && <Loader />}
-        <img
-          className={css.img}
-          src={`https://image.tmdb.org/t/p/w300/${image}`}
-          alt={title}
-          width={300}
-        ></img>
-        <div className={css.description}>
-          <h1>{`${title} (${date})`}</h1>
-          <p className={css.score}>User Score: {score}%</p>
-          <h2>Overview</h2>
-          <p className={css.overview}>{overview}</p>
-          <h2>Genres</h2>
-          <p>{genresList}</p>
-          <p className={css.additional}>Additional information</p>
-          <ul className={css.list}>
-            <li className={css.item}>
-              <Link className={css.link} to="cast">
-                Cast
-              </Link>
-            </li>
-            <li className={css.item}>
-              <Link className={css.link} to="reviews">
-                Reviews
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-
+      {error && <p>{error}</p>}
+      {isLoading && <Loader />}
+      {movieInfo && <Info movieInfo={movieInfo} />}
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
